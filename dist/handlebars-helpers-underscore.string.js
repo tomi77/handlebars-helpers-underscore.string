408056bce37932b8eb20b86cb57f37b62ff587eb
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-07-19 11:06:22 +0200

    fix description

--- dist/handlebars-helpers-underscore.string.js
+++ dist/handlebars-helpers-underscore.string.js
@@ -1,10 +1,10 @@
 
 /*
-  Backbone-Forms Bootstrap TypeAhead editor 1.0.0
+  Handlebars helpers underscore.string 1.0.0
 
-  Copyright (c) 2016 Tomasz Jakub Rup
+  Copyright (c) 2014-2016 Tomasz Jakub Rup
 
-  https://github.com/tomi77/backbone-forms-typeahead
+  https://github.com/tomi77/handlebars-helpers-underscore.string
 
   Released under the MIT license
  */
