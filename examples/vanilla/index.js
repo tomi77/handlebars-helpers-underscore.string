2847b07983f1b2a3c784970d5354d55fc5b3205a
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-03-13 20:57:01 +0100

    add next example

---
+++ examples/vanilla/index.js
@@ -0,0 +1,10 @@
+var source = "<p>Hello, my name is {{name}}. I am from {{underscored hometown}}. I have " +
+             "{{kids.length}} kids:</p>" +
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+var template = Handlebars.compile(source);
+
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+var result = template(data);
+
+document.getElementsByTagName('div')[0].innerHTML = result;
