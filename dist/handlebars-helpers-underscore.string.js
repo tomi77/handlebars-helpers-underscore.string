17f935510492c2a23471cf1856600d7d27be3988
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-05-22 12:22:01 +0200

    Simplify code

--- dist/handlebars-helpers-underscore.string.js
+++ dist/handlebars-helpers-underscore.string.js
@@ -22,279 +22,72 @@ var slice = [].slice;
       factory(root.Handlebars, root._.str);
   }
 })(this, function(Handlebars, _s) {
-  Handlebars.registerHelper('is_blank', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.isBlank.apply(null, args);
-  });
-  Handlebars.registerHelper('strip_tags', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.stripTags.apply(null, args);
-  });
-  Handlebars.registerHelper('capitalize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.capitalize.apply(null, args);
-  });
-  Handlebars.registerHelper('chop', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.chop.apply(null, args);
-  });
-  Handlebars.registerHelper('clean', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.clean.apply(null, args);
-  });
-  Handlebars.registerHelper('count', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.count.apply(null, args);
-  });
-  Handlebars.registerHelper('chars', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.chars.apply(null, args);
-  });
-  Handlebars.registerHelper('swap_case', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.swapCase.apply(null, args);
-  });
-  Handlebars.registerHelper('splice', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.splice.apply(null, args);
-  });
-  Handlebars.registerHelper('insert', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.insert.apply(null, args);
-  });
-  Handlebars.registerHelper('include', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.include.apply(null, args);
-  });
-  Handlebars.registerHelper('join', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.join.apply(null, args);
-  });
-  Handlebars.registerHelper('lines', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.lines.apply(null, args);
-  });
-  Handlebars.registerHelper('reverse', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.reverse.apply(null, args);
-  });
-  Handlebars.registerHelper('starts_with', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.startsWith.apply(null, args);
-  });
-  Handlebars.registerHelper('ends_with', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.endsWith.apply(null, args);
-  });
-  Handlebars.registerHelper('succ', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.succ.apply(null, args);
-  });
-  Handlebars.registerHelper('titleize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.titleize.apply(null, args);
-  });
-  Handlebars.registerHelper('camelize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.camelize.apply(null, args);
-  });
-  Handlebars.registerHelper('underscored', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.underscored.apply(null, args);
-  });
-  Handlebars.registerHelper('dasherize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.dasherize.apply(null, args);
-  });
-  Handlebars.registerHelper('classify', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.classify.apply(null, args);
-  });
-  Handlebars.registerHelper('humanize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.humanize.apply(null, args);
-  });
-  Handlebars.registerHelper('trim', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.trim.apply(null, args);
-  });
-  Handlebars.registerHelper('ltrim', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.ltrim.apply(null, args);
-  });
-  Handlebars.registerHelper('rtrim', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.rtrim.apply(null, args);
-  });
-  Handlebars.registerHelper('truncate', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.truncate.apply(null, args);
-  });
-  Handlebars.registerHelper('prune', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.prune.apply(null, args);
-  });
-  Handlebars.registerHelper('words', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.words.apply(null, args);
-  });
-  Handlebars.registerHelper('pad', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.pad.apply(null, args);
-  });
-  Handlebars.registerHelper('lpad', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.lpad.apply(null, args);
-  });
-  Handlebars.registerHelper('rpad', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.rpad.apply(null, args);
-  });
-  Handlebars.registerHelper('lrpad', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.lrpad.apply(null, args);
-  });
-  Handlebars.registerHelper('sprintf', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.sprintf.apply(null, args);
-  });
-  Handlebars.registerHelper('vsprintf', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.vsprintf.apply(null, args);
-  });
-  Handlebars.registerHelper('to_number', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.toNumber.apply(null, args);
-  });
-  Handlebars.registerHelper('number_format', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.numberFormat.apply(null, args);
-  });
-  Handlebars.registerHelper('str_right', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.strRight.apply(null, args);
-  });
-  Handlebars.registerHelper('str_right_back', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.strRightBack.apply(null, args);
-  });
-  Handlebars.registerHelper('str_left', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.strLeft.apply(null, args);
-  });
-  Handlebars.registerHelper('str_left_back', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.strLeftBack.apply(null, args);
-  });
-  Handlebars.registerHelper('to_sentence', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.toSentence.apply(null, args);
-  });
-  Handlebars.registerHelper('to_sentence_serial', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.toSentenceSerial.apply(null, args);
-  });
-  Handlebars.registerHelper('slugify', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.slugify.apply(null, args);
-  });
-  Handlebars.registerHelper('surround', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.surround.apply(null, args);
-  });
-  Handlebars.registerHelper('quote', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.quote.apply(null, args);
-  });
-  Handlebars.registerHelper('unquote', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.unquote.apply(null, args);
-  });
-  Handlebars.registerHelper('repeat', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.repeat.apply(null, args);
-  });
-  Handlebars.registerHelper('levenshtein', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.levenshtein.apply(null, args);
-  });
-  Handlebars.registerHelper('to_boolean', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.toBoolean.apply(null, args);
-  });
-  Handlebars.registerHelper('clean_diacritics', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.cleanDiacritics.apply(null, args);
-  });
-  Handlebars.registerHelper('decapitalize', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.decapitalize.apply(null, args);
-  });
-  Handlebars.registerHelper('dedent', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.dedent.apply(null, args);
-  });
-  Handlebars.registerHelper('pred', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.pred.apply(null, args);
-  });
-  Handlebars.registerHelper('replace_all', function() {
-    var args, i, options;
-    args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
-    return _s.replaceAll.apply(null, args);
-  });
+  var func, helper, helpers;
+  helpers = {
+    camelize: _s.camelize,
+    capitalize: _s.capitalize,
+    chars: _s.chars,
+    chop: _s.chop,
+    classify: _s.classify,
+    clean: _s.clean,
+    clean_diacritics: _s.cleanDiacritics,
+    count: _s.count,
+    decapitalize: _s.decapitalize,
+    dedent: _s.dedent,
+    dasherize: _s.dasherize,
+    ends_with: _s.endsWith,
+    humanize: _s.humanize,
+    include: _s.include,
+    insert: _s.insert,
+    is_blank: _s.isBlank,
+    join: _s.join,
+    levenshtein: _s.levenshtein,
+    lines: _s.lines,
+    lpad: _s.lpad,
+    lrpad: _s.lrpad,
+    ltrim: _s.ltrim,
+    number_format: _s.numberFormat,
+    pad: _s.pad,
+    pred: _s.pred,
+    prune: _s.prune,
+    quote: _s.quote,
+    repeat: _s.repeat,
+    replace_all: _s.replaceAll,
+    reverse: _s.reverse,
+    rpad: _s.rpad,
+    rtrim: _s.rtrim,
+    slugify: _s.slugify,
+    splice: _s.splice,
+    sprintf: _s.sprintf,
+    starts_with: _s.startsWith,
+    strip_tags: _s.stripTags,
+    str_left: _s.strLeft,
+    str_left_back: _s.strLeftBack,
+    str_right: _s.strRight,
+    str_right_back: _s.strRightBack,
+    succ: _s.succ,
+    surround: _s.surround,
+    swap_case: _s.swapCase,
+    titleize: _s.titleize,
+    to_boolean: _s.toBoolean,
+    to_number: _s.toNumber,
+    to_sentence: _s.toSentence,
+    to_sentence_serial: _s.toSentenceSerial,
+    trim: _s.trim,
+    truncate: _s.truncate,
+    underscored: _s.underscored,
+    unquote: _s.unquote,
+    vsprintf: _s.vsprintf,
+    words: _s.words
+  };
+  for (helper in helpers) {
+    func = helpers[helper];
+    Handlebars.registerHelper(helper, (function(func) {
+      return function() {
+        var args, i, options;
+        args = 2 <= arguments.length ? slice.call(arguments, 0, i = arguments.length - 1) : (i = 0, []), options = arguments[i++];
+        return func.apply(null, args);
+      };
+    })(func));
+  }
 });
--- src/handlebars-helpers-underscore.string.coffee
+++ src/handlebars-helpers-underscore.string.coffee
@@ -18,114 +18,66 @@
       factory root.Handlebars, root._.str
   return
 ) @, (Handlebars, _s) ->
-  Handlebars.registerHelper 'is_blank', (args..., options) -> _s.isBlank.apply null, args
+  helpers =
+    camelize: _s.camelize
+    capitalize: _s.capitalize
+    chars: _s.chars
+    chop: _s.chop
+    classify: _s.classify
+    clean: _s.clean
+    clean_diacritics: _s.cleanDiacritics
+    count: _s.count
+    decapitalize: _s.decapitalize
+    dedent: _s.dedent
+    dasherize: _s.dasherize
+    ends_with: _s.endsWith
+    humanize: _s.humanize
+    include: _s.include
+    insert: _s.insert
+    is_blank: _s.isBlank
+    join: _s.join
+    levenshtein: _s.levenshtein
+    lines: _s.lines
+    lpad: _s.lpad
+    lrpad: _s.lrpad
+    ltrim: _s.ltrim
+    number_format: _s.numberFormat
+    pad: _s.pad
+    pred: _s.pred
+    prune: _s.prune
+    quote: _s.quote
+    repeat: _s.repeat
+    replace_all: _s.replaceAll
+    reverse: _s.reverse
+    rpad: _s.rpad
+    rtrim: _s.rtrim
+    slugify: _s.slugify
+    splice: _s.splice
+    sprintf: _s.sprintf
+    starts_with: _s.startsWith
+    strip_tags: _s.stripTags
+    str_left: _s.strLeft
+    str_left_back: _s.strLeftBack
+    str_right: _s.strRight
+    str_right_back: _s.strRightBack
+    succ: _s.succ
+    surround: _s.surround
+    swap_case: _s.swapCase
+    titleize: _s.titleize
+    to_boolean: _s.toBoolean
+    to_number: _s.toNumber
+    to_sentence: _s.toSentence
+    to_sentence_serial: _s.toSentenceSerial
+    trim: _s.trim
+    truncate: _s.truncate
+    underscored: _s.underscored
+    unquote: _s.unquote
+    vsprintf: _s.vsprintf
+    words: _s.words
 
-  Handlebars.registerHelper 'strip_tags', (args..., options) -> _s.stripTags.apply null, args
-
-  Handlebars.registerHelper 'capitalize', (args..., options) -> _s.capitalize.apply null, args
-
-  Handlebars.registerHelper 'chop', (args..., options) -> _s.chop.apply null, args
-
-  Handlebars.registerHelper 'clean', (args..., options) -> _s.clean.apply null, args
-
-  Handlebars.registerHelper 'count', (args..., options) -> _s.count.apply null, args
-
-  Handlebars.registerHelper 'chars', (args..., options) -> _s.chars.apply null, args
-
-  Handlebars.registerHelper 'swap_case', (args..., options) -> _s.swapCase.apply null, args
-
-  Handlebars.registerHelper 'splice', (args..., options) -> _s.splice.apply null, args
-
-  Handlebars.registerHelper 'insert', (args..., options) -> _s.insert.apply null, args
-
-  Handlebars.registerHelper 'include', (args..., options) -> _s.include.apply null, args
-
-  Handlebars.registerHelper 'join', (args..., options) -> _s.join.apply null, args
-
-  Handlebars.registerHelper 'lines', (args..., options) -> _s.lines.apply null, args
-
-  Handlebars.registerHelper 'reverse', (args..., options) -> _s.reverse.apply null, args
-
-  Handlebars.registerHelper 'starts_with', (args..., options) -> _s.startsWith.apply null, args
-
-  Handlebars.registerHelper 'ends_with', (args..., options) -> _s.endsWith.apply null, args
-
-  Handlebars.registerHelper 'succ', (args..., options) -> _s.succ.apply null, args
-
-  Handlebars.registerHelper 'titleize', (args..., options) -> _s.titleize.apply null, args
-
-  Handlebars.registerHelper 'camelize', (args..., options) -> _s.camelize.apply null, args
-
-  Handlebars.registerHelper 'underscored', (args..., options) -> _s.underscored.apply null, args
-
-  Handlebars.registerHelper 'dasherize', (args..., options) -> _s.dasherize.apply null, args
-
-  Handlebars.registerHelper 'classify', (args..., options) -> _s.classify.apply null, args
-
-  Handlebars.registerHelper 'humanize', (args..., options) -> _s.humanize.apply null, args
-
-  Handlebars.registerHelper 'trim', (args..., options) -> _s.trim.apply null, args
-
-  Handlebars.registerHelper 'ltrim', (args..., options) -> _s.ltrim.apply null, args
-
-  Handlebars.registerHelper 'rtrim', (args..., options) -> _s.rtrim.apply null, args
-
-  Handlebars.registerHelper 'truncate', (args..., options) -> _s.truncate.apply null, args
-
-  Handlebars.registerHelper 'prune', (args..., options) -> _s.prune.apply null, args
-
-  Handlebars.registerHelper 'words', (args..., options) -> _s.words.apply null, args
-
-  Handlebars.registerHelper 'pad', (args..., options) -> _s.pad.apply null, args
-
-  Handlebars.registerHelper 'lpad', (args..., options) -> _s.lpad.apply null, args
-
-  Handlebars.registerHelper 'rpad', (args..., options) -> _s.rpad.apply null, args
-
-  Handlebars.registerHelper 'lrpad', (args..., options) -> _s.lrpad.apply null, args
-
-  Handlebars.registerHelper 'sprintf', (args..., options) -> _s.sprintf.apply null, args
-
-  Handlebars.registerHelper 'vsprintf', (args..., options) -> _s.vsprintf.apply null, args
-
-  Handlebars.registerHelper 'to_number', (args..., options) -> _s.toNumber.apply null, args
-
-  Handlebars.registerHelper 'number_format', (args..., options) -> _s.numberFormat.apply null, args
-
-  Handlebars.registerHelper 'str_right', (args..., options) -> _s.strRight.apply null, args
-
-  Handlebars.registerHelper 'str_right_back', (args..., options) -> _s.strRightBack.apply null, args
-
-  Handlebars.registerHelper 'str_left', (args..., options) -> _s.strLeft.apply null, args
-
-  Handlebars.registerHelper 'str_left_back', (args..., options) -> _s.strLeftBack.apply null, args
-
-  Handlebars.registerHelper 'to_sentence', (args..., options) -> _s.toSentence.apply null, args
-
-  Handlebars.registerHelper 'to_sentence_serial', (args..., options) -> _s.toSentenceSerial.apply null, args
-
-  Handlebars.registerHelper 'slugify', (args..., options) -> _s.slugify.apply null, args
-
-  Handlebars.registerHelper 'surround', (args..., options) -> _s.surround.apply null, args
-
-  Handlebars.registerHelper 'quote', (args..., options) -> _s.quote.apply null, args
-
-  Handlebars.registerHelper 'unquote', (args..., options) -> _s.unquote.apply null, args
-
-  Handlebars.registerHelper 'repeat', (args..., options) -> _s.repeat.apply null, args
-
-  Handlebars.registerHelper 'levenshtein', (args..., options) -> _s.levenshtein.apply null, args
-
-  Handlebars.registerHelper 'to_boolean', (args..., options) -> _s.toBoolean.apply null, args
-
-  Handlebars.registerHelper 'clean_diacritics', (args..., options) -> _s.cleanDiacritics.apply null, args
-
-  Handlebars.registerHelper 'decapitalize', (args..., options) -> _s.decapitalize.apply null, args
-
-  Handlebars.registerHelper 'dedent', (args..., options) -> _s.dedent.apply null, args
-
-  Handlebars.registerHelper 'pred', (args..., options) -> _s.pred.apply null, args
-
-  Handlebars.registerHelper 'replace_all', (args..., options) -> _s.replaceAll.apply null, args
+  for helper, func of helpers
+    Handlebars.registerHelper helper, ((func) ->
+      (args..., options) -> func.apply null, args
+    )(func)
 
   return
